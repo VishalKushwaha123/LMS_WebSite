00c1cd195c85579acdb144b26e07cbc9b76ec1d9
Vishal Kushwaha <vishal818799@gmail.com>
2025-08-24 22:41:34 +0530

    fdg

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,54 +1,52 @@
 import { Webhook } from "svix";
 import User from "../models/User.js";
 
-export const clearkWebhooks = async (req, res) => {
+//API Controller Function to Manage clerk user with database
+
+export const clerkWebhooks = async (req, res) => {
   try {
-    const webhook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
-    // Verify webhook
-    await webhook.verify(JSON.stringify(req.body), {
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+    await whook.verify(JSON.stringify(req.body), {
       "svix-id": req.headers["svix-id"],
       "svix-timestamp": req.headers["svix-timestamp"],
       "svix-signature": req.headers["svix-signature"],
     });
+
     const { data, type } = req.body;
+
     switch (type) {
       case "user.created": {
-        // Handle user created event
         const userData = {
           _id: data.id,
           email: data.email_addresses[0].email_address,
           name: data.first_name + " " + data.last_name,
           imageUrl: data.image_url,
-          // Add any other relevant user fields
         };
         await User.create(userData);
-        res.json({ message: "User created successfully", userData });
+        res.json({});
         break;
       }
+
       case "user.updated": {
-        // Handle user updated event
         const userData = {
           email: data.email_addresses[0].email_address,
           name: data.first_name + " " + data.last_name,
           imageUrl: data.image_url,
         };
         await User.findByIdAndUpdate(data.id, userData);
-        res.json({ message: "User updated successfully", userData });
+        res.json({});
         break;
       }
       case "user.deleted": {
-        // Handle user deleted event
         await User.findByIdAndDelete(data.id);
-        res.json({ message: "User deleted successfully" });
+        res.json({});
         break;
       }
-      default: {
-        console.warn("Unhandled webhook event type:", type);
+      default:
         break;
-      }
     }
   } catch (error) {
-    console.error("Error registering webhook:", error);
-    res.status(500).json({ message: "Internal server error Webhook" });
+    res.json({ success: false, message: error.message });
   }
 };
