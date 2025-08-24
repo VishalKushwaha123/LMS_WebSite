97ac4171b693eab8b8ea75e7d94fc865a143939d
Vishal Kushwaha <vishal818799@gmail.com>
2025-08-24 22:26:02 +0530

    gvgvg

--- server/server.js
+++ server/server.js
@@ -1,26 +1,22 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './configs/mongodb.js'
-import { clerkWebhooks } from './controllers/webhooks.js'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/mongodb.js";
+import { clerkWebhooks } from "./controllers/webhooks.js";
 
 //initilize Express
-const app = express()
+const app = express();
 
 //connect to database
-await connectDB()
+await connectDB();
 
 //middleware
-app.use(cors())
+app.use(cors());
 
 //Routes
-app.get('/', (req, res) => 
-    res.send('Hello from server')
-)
-app.post('/clerk', express.json(), clerkWebhooks)
+app.get("/", (req, res) => res.send("Hello from server"));
+app.post("/clerk", express.json(), clerkWebhooks);
 
-//port 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => 
-    console.log(`Server is running on port ${PORT}`)
-)
+//port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
