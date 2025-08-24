9995755c10622b96dcde7226f60cfffa837e9301
Vishal Kushwaha <vishal818799@gmail.com>
2025-08-24 22:28:14 +0530

    njn

--- server/server.js
+++ server/server.js
@@ -18,5 +18,4 @@ app.get("/", (req, res) => res.send("Hello from server"));
 app.post("/clerk", express.json(), clerkWebhooks);
 
 //port
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+export default app;
